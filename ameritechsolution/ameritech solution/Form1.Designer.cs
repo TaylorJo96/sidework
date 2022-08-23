98131c748c32eddb33afde9bfe677bfe75f1c879
Taylor Dupont <taylorjodupont96@gmail.com>
2022-08-23 15:47:25 -0400

    adding finished project to git

---
+++ ameritechsolution/ameritech solution/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿
+namespace ameritech_solution
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Font = new System.Drawing.Font("Karmatic Arcade", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(274, 100);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(250, 107);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Select CSV file";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Palatino Linotype", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(250, 274);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(305, 72);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(262, 258);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(274, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "The last 10 digits of the sum  of the lines in the csv file is:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ameritechsolution/ameritech solution/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Security;
+
+namespace ameritech_solution
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            long  total = 0;
+           
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "CSV Files (*.csv)|*.csv";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+            
+                    filePath = openFileDialog.FileName;
+
+                    //Read the contents of the file into a stream
+                    var fileStream = openFileDialog.OpenFile();
+
+                    using (StreamReader reader = new StreamReader(fileStream))
+                    {
+                        string line;
+                       
+
+                        // I decided to read the document one line at a time so the streamreader would not be overloaded with a ton of information
+                        while ((line = reader.ReadLine()) != null)
+                        {
+                            try
+                            {
+                                if (line.Length > 10)
+                                {
+                                    line = line.Substring(line.Length - 11, 10);
+
+                                    //I would like to do more testing to insure the accuracy of the results but by taking out these extra high digits I can make the numbers handleable
+
+                                    long result = Int64.Parse(line);
+                                   
+                                    total = total + result;
+                                }
+                                else 
+                                {
+                                    int result = Int32.Parse(line);
+                                    total = total + result;
+                                }
+                            
+                            }
+                            
+                            catch (FormatException)
+                            {
+
+                            
+                                continue;
+                            }
+
+                        }
+                    }
+                }
+                string longAnswer = total.ToString();
+                string finalanswer = "";
+                if (longAnswer.Length > 10)
+                {
+                   finalanswer = longAnswer.Substring(longAnswer.Length - 11, 10);
+                }
+                else 
+                {
+                     finalanswer = longAnswer;
+                   
+                }
+         
+
+                textBox1.Text = finalanswer;
+            }
+
+
+        }
+        public class OpenFileDialogForm : Form
+        {
+
+
+            private Button selectButton;
+            private OpenFileDialog openFileDialog1;
+            private TextBox textBox1;
+
+            public OpenFileDialogForm()
+            {
+                openFileDialog1 = new OpenFileDialog();
+                selectButton = new Button
+                {
+                    Size = new Size(100, 20),
+                    Location = new Point(15, 15),
+                    Text = "Select file"
+                };
+                selectButton.Click += new EventHandler(SelectButton_Click);
+                textBox1 = new TextBox
+                {
+                    Size = new Size(300, 300),
+                    Location = new Point(15, 40),
+                    Multiline = true,
+                    ScrollBars = ScrollBars.Vertical
+                };
+                ClientSize = new Size(330, 360);
+                Controls.Add(selectButton);
+                Controls.Add(textBox1);
+            }
+            private void SetText(string text)
+            {
+                textBox1.Text = text;
+            }
+            private void SelectButton_Click(object sender, EventArgs e)
+            {
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        var sr = new StreamReader(openFileDialog1.FileName);
+                        SetText(sr.ReadToEnd());
+                    }
+                    catch (SecurityException ex)
+                    {
+                        MessageBox.Show($"Security error.\n\nError message: {ex.Message}\n\n" +
+                        $"Details:\n\n{ex.StackTrace}");
+                    }
+                }
+            }
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
